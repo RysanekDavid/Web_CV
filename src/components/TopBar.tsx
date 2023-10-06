55e0f2a8bbc9a9e8b906b85ba097e1a17fb43407
Ryšánek David <rysanek.david.2@cpost.cz>
2023-10-06 16:30:07 +0200

    TopBar Design update

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,13 +3,20 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-// import Tooltip from "@mui/material/Tooltip"
-// import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+// import { createTheme } from "@mui/system";
 
 const pages = ["About Me", "Resume", "Projects"];
 
-function ResponsiveAppBar() {
+function ResponsiveTopBar() {
+  const [selectedTab, setSelectedTab] = React.useState(0);
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setSelectedTab(newValue);
+  };
+
   return (
     <AppBar
       position="static"
@@ -21,25 +28,72 @@ function ResponsiveAppBar() {
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => {}}
-                sx={{
-                  my: 2,
-                  color: "white",
-                  display: "block",
-                  fontSize: "16px",
-                }}
-              >
-                {page}
-              </Button>
-            ))}
+          <Box
+            sx={{
+              flexGrow: 1,
+
+              display: {
+                xs: "flex",
+                sm: "flex",
+                md: "flex",
+                lg: "flex",
+                xl: "flex",
+              },
+              position: "relative",
+
+              justifyContent: {
+                xs: "center",
+                sm: "center",
+                md: "left",
+                lg: "left",
+                xl: "left",
+              },
+            }}
+          >
+            <Tabs
+              value={selectedTab}
+              onChange={handleTabChange}
+              centered={true}
+              textColor="inherit"
+              indicatorColor="primary"
+              sx={{
+                "& .MuiTabs-indicator": {
+                  top: 0,
+                  display: "none",
+                },
+              }}
+            >
+              {pages.map((page, index) => (
+                <Tab
+                  key={index}
+                  label={page}
+                  sx={{
+                    backgroundColor:
+                      selectedTab === index ? "#121212" : "#1E1E1E",
+                    my: 1,
+                    mx: 1,
+                    whiteSpace: "nowrap",
+                    borderTop: selectedTab === index ? 3 : 0,
+                    borderLeft: selectedTab === index ? 3 : 0,
+                    borderRight: selectedTab === index ? 3 : 0,
+
+                    borderColor: "#29962c",
+                    fontSize: {
+                      xl: "18px",
+                      lg: "18px",
+                      md: "16px",
+                      sm: "16px",
+                      xs: "16px",
+                    },
+                    fontFamily: "Roboto Slab",
+                  }}
+                />
+              ))}
+            </Tabs>
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
-export default ResponsiveAppBar;
+export default ResponsiveTopBar;
