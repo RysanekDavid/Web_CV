b3830c0552c908b658176ba354bafe4a03492b0a
Ryšánek David <rysanek.david.2@cpost.cz>
2023-10-10 18:22:10 +0200

    AboutMe Update, responsivity changes

--- src/components/ContentBar.tsx
+++ src/components/ContentBar.tsx
@@ -4,16 +4,32 @@ import {
   Typography,
   Avatar,
   List,
-  ListItem,
-  ListItemText,
   Grid,
   Card,
   CardContent,
 } from "@mui/material";
+import { useEffect, useState, useMemo } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import resumeImage from "../assets/CV - David Ryšánek.jpg";
 import AvatarImage from "../assets/avatar1.jpg";
 
 function AboutMe() {
+  const [currentProfession, setCurrentProfession] = useState("Developer");
+  const professions = useMemo(() => ["Developer", "Designer", "Analyst"], []);
+  const [professionIndex, setProfessionIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProfessionIndex((prevIndex) => (prevIndex + 1) % professions.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setCurrentProfession(professions[professionIndex]);
+  }, [professionIndex, professions]);
+
   return (
     <Box
       sx={{
@@ -26,34 +42,50 @@ function AboutMe() {
         src={AvatarImage}
         alt="David Ryšánek fotka"
         sx={{
-          width: 250,
-          height: 250,
+          width: { xl: 250, lg: 250, md: 220, sm: 200, xs: 180 },
+          height: { xl: 250, lg: 250, md: 220, sm: 200, xs: 180 },
           mb: 2,
-          top: 20,
+          top: 80,
           border: "4px solid #29962c",
         }}
       />
+
+      <Box sx={{ height: "50px", position: "relative" }}>
+        <TransitionGroup>
+          <CSSTransition
+            key={currentProfession}
+            timeout={600}
+            classNames="slide"
+          >
+            <Typography sx={{ color: "#29962c", paddingTop: 12 }} variant="h4">
+              {currentProfession}
+            </Typography>
+          </CSSTransition>
+        </TransitionGroup>
+      </Box>
+
       <Typography
         sx={{
           color: "#FFFFFF",
           justifyContent: "center",
-          paddingLeft: 50,
-          paddingRight: 50,
-          paddingTop: 2,
+          paddingLeft: { xl: 40, lg: 25, md: 16, sm: 10, xs: 4 },
+          paddingRight: { xl: 40, lg: 25, md: 16, sm: 10, xs: 4 },
+          paddingTop: 16,
           fontFamily: "Fira Code",
-          fontSize: "18px",
+          fontSize: "19px",
         }}
-        variant="subtitle1"
+        variant="h6"
       >
         In over 2 years in the digital field, I've mastered web development and
         graphic design. I create websites that are not only visually appealing
         but also user-focused and functional. My passion for graphic design
         translates into captivating visuals that effectively convey messages.
       </Typography>
+
       <Typography
         sx={{
           color: "#29962c",
-          paddingTop: 60,
+          paddingTop: 45,
           fontFamily: "Roboto Slab",
           fontSize: "36px",
         }}
@@ -61,20 +93,32 @@ function AboutMe() {
       >
         About Me
       </Typography>
-      Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Eum Voluptatibus
-      Dolores Aspernatur Animi Perferendis Iste! Culpa Aut Enim Debitis Optio
-      Illum Ipsum Quis Perferendis Dolor Expedita. Similique Necessitatibus
-      Inventore Doloremque.
+
       <Typography
         sx={{
           color: "#29962c",
-          paddingTop: 2,
+          paddingTop: 10,
           fontFamily: "Fira Code",
           fontSize: "30px",
         }}
         variant="h4"
       >
         My Name Is David
+      </Typography>
+
+      <Typography
+        sx={{
+          color: "#29962c",
+          paddingTop: 5,
+          fontFamily: "Fira Code",
+          fontSize: "20px",
+        }}
+        variant="h4"
+      >
+        Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Eum
+        Voluptatibus Dolores Aspernatur Animi Perferendis Iste! Culpa Aut Enim
+        Debitis Optio Illum Ipsum Quis Perferendis Dolor Expedita. Similique
+        Necessitatibus Inventore Doloremque.
       </Typography>
     </Box>
   );
@@ -112,7 +156,6 @@ function Projects() {
             </CardContent>
           </Card>
         </Grid>
-        // Přidejte další projekty podle potřeby...
       </Grid>
     </Box>
   );
