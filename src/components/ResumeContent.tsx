437c79015e60fbffa2bd0e65f638406076434fa1
DavidRysanek <dawelich@gmail.com>
2023-11-16 21:52:28 +0100

    Device switch

--- src/components/ResumeContent.tsx
+++ src/components/ResumeContent.tsx
@@ -4,7 +4,7 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#1E1E1E",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
@@ -14,8 +14,8 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Resume() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} sx={{ backgroundColor: "black" }}>
-        <Grid xs={8} sx={{ color: "white" }}>
+      <Grid container spacing={2} sx={{}}>
+        <Grid xs={8} sx={{}}>
           <Item>Thick Bracket</Item>
         </Grid>
         <Grid xs={4}>
