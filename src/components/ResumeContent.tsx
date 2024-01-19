a4711335fca2facdf8fb8ce897eae35e046b7f6b
DavidRysanek <dawelich@gmail.com>
2024-01-19 13:07:38 +0100

    Device add

--- src/components/ResumeContent.tsx
+++ src/components/ResumeContent.tsx
@@ -1,39 +1,42 @@
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
+import { Box, Paper, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#1E1E1E",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
   color: theme.palette.text.secondary,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  fontFamily: "Arial, sans-serif",
 }));
 
 export default function Resume() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} sx={{}}>
-        <Grid xs={8} sx={{}}>
-          <Item>Thick Bracket</Item>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Typography variant="h5">Resume</Typography>
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+          <Grid item xs={6}>
+            <Item>
+              <h2>Professional Experience</h2>
+            </Item>
+            <Item sx={{ mt: 2 }}>
+              <h2>Education</h2>
+            </Item>
+            <Item sx={{ mt: 2 }}>
+              <h2>Contact Information</h2>
+            </Item>
+            <Item sx={{ mt: 2 }}>
+              <h2>Skills</h2>
+            </Item>
+            <Item sx={{ mt: 2 }}>
+              <h2>Certifications & Languages</h2>
+            </Item>
+          </Grid>
         </Grid>
-        <Grid xs={4}>
-          <Item>Thin Bracket</Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>Second Thin</Item>
-        </Grid>
-        <Grid xs={8}>
-          <Item>Second Thick</Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>Second Thin</Item>
-        </Grid>
-        <Grid xs={8}>
-          <Item>Second Thick</Item>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 }
