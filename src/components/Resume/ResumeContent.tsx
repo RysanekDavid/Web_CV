cb921ed6695511c9df3766dba5e4d0c809b4d37a
DavidRysanek <dawelich@gmail.com>
2024-03-13 02:00:09 +0100

    Education Card started

--- src/components/Resume/ResumeContent.tsx
+++ src/components/Resume/ResumeContent.tsx
@@ -9,15 +9,19 @@ import pageTittle from "../../styles/ResumePage/PageTittle";
 import regularText from "../../styles/ResumePage/RegularText";
 import CardTitle from "../../styles/ResumePage/Card/CardTitle";
 import ItemStyle from "../../styles/ResumePage/Card/ItemStyle";
+import UniversityIcon from "../../assets/ResumeIcons/university_icon.svg";
+import HighSchoolIcon from "../../assets/ResumeIcons/high_school_icon.svg";
+import iconsStyles from "../../styles/ResumePage/Card/IconsStyle";
+
 export default function Resume() {
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <Typography sx={pageTittle}>Resume</Typography>
       </Box>
-      <Box sx={{ flexGrow: 1, pt: 4 }}>
-        <Grid container spacing={6} sx={{ justifyContent: "center" }}>
-          <Grid item xs={12} sm={6} md={4} lg={5}>
+      <Box sx={{ flexGrow: 1, pt: 3, pr: "1rem", pl: "1rem" }}>
+        <Grid container spacing={4} sx={{ justifyContent: "center" }}>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
             <ItemStyle>
               <CardTitle>Professional Experience</CardTitle>
               <Typography sx={ProfesionalExperienceText}>
@@ -47,6 +51,22 @@ export default function Resume() {
           <Grid item xs={12} sm={6} md={4} lg={5}>
             <ItemStyle>
               <CardTitle>Education</CardTitle>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src={HighSchoolIcon}
+                  alt="High school"
+                  style={iconsStyles}
+                />
+                <Typography sx={{ pl: "1rem" }}>My highSchool</Typography>
+              </Box>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src={UniversityIcon}
+                  alt="University"
+                  style={iconsStyles}
+                />
+                <Typography sx={{ pl: "1rem" }}>My university</Typography>
+              </Box>
             </ItemStyle>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={5}>
