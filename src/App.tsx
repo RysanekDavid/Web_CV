a462ce3f5c0c92125cb02851a6573781670dadd1
DavidRysanek <dawelich@gmail.com>
2024-04-22 23:01:27 +0200

    Swipe option added

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,19 @@ import BottomBar from "./components/General/BottomBar";
 import { AboutMe } from "./pages/AboutMeContent";
 import Resume from "./pages/ResumeContent";
 import Projects from "./pages/ProjectContent";
+import { useSwipeable } from "react-swipeable";
 
 function App() {
   const [selectedTab, setSelectedTab] = React.useState(0);
 
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => setSelectedTab((prev) => (prev < 2 ? prev + 1 : prev)),
+    onSwipedRight: () => setSelectedTab((prev) => (prev > 0 ? prev - 1 : prev)),
+    trackMouse: false, // Pouze pro touch zařízení
+  });
+
   return (
-    <div className="App">
+    <div className="App" {...swipeHandlers}>
       <TopBar selectedTab={selectedTab} onTabChange={setSelectedTab} />
       <BottomBar />
       <div className="content">
